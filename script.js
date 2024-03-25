733e4718f36939153697ada4c6dfc0729ab18912
KrishiDevani15 <krishiminergate@gmail.com>
2024-03-25 15:47:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js">
+    var typed = new Typed(".auto-type",(
+        strings : ["Python Developer","Data Scientist","Data Analyst"],
+        typeSpeed:150,
+        backSpeed:150,
+        looped:true
+    ))
+
+</script>
